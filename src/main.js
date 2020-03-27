1180e8aa33127c08fe46897953486bbaf4800978
Pablo Bejarano <pabhoz@gmail.com>
2020-03-27 13:39:36 -0500

    Adicionada la clase isDone when is checked

--- src/main.js
+++ src/main.js
@@ -3,11 +3,23 @@ addNoteBtn.onclick = () => {
     let newNoteInput = document.querySelector('#newNoteInput');
     let notes = document.querySelector('#app .notes');
     let note = document.createElement('li');
+
     let checkBox = document.createElement('input');
     checkBox.type = "checkbox";
+    checkBox.onclick = function() {
+        let parent = this.parentElement;
+        if (this.checked) {
+            parent.classList.add('isDone');
+        } else {
+            parent.classList.remove('isDone');
+        }
+    };
+
     note.classList.add('note');
-    note.innerHTML = newNoteInput.value;
+    note.innerHTML = `<div>${newNoteInput.value}</div>`;
     note.appendChild(checkBox);
+
     newNoteInput.value = '';
+
     notes.appendChild(note);
 };
